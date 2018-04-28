71cacfffdae8de81b58f013a7e03ef26d930b8db
facheng.wang <facheng.wang@otms.com>
2018-04-28 13:03:29 +0800

    wx bot test.

---
+++ wecaht_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding: utf-8
+
+'''
+install wxpy first:
+  sudo pip install wxpy
+'''
+import sys
+from wxpy import *
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+print sys.getdefaultencoding()
+
+bot = Bot()
+target_group = bot.groups().search('忽悠小分队'.encode('utf-8').decode('utf-8'))[0]
+
+tuling = Tuling(api_key='2782745ec4eb4aa1be87d121417fb8d8')
+
+@bot.register(target_group)
+def print_msg(msg):
+    print msg
+    return tuling.do_reply(msg)
+
+embed()
