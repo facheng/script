1030986047ec63df111f9ed25a9c3ab089fb4ef4
facheng.wang <facheng.wang@otms.cn>
2018-08-25 02:26:42 +0800

    code modify.

--- jd_extension/manager.js
+++ jd_extension/manager.js
@@ -3,19 +3,100 @@
 // found in the LICENSE file.
 
 'use strict';
+
+$.extend({
+    syncPost: function(request_url, request_data, callback) {
+        $.ajax({
+            type: 'POST',
+            url: request_url,
+            data: request_data,
+            async: false,
+            success: function(response) {
+                callback(response)
+            }
+        })
+    }
+});
+
+$.extend({
+    myPost: function(request_data, request_sms, callback) {
+        if (request_sms) {
+            $.syncPost('https://coin.jd.com/sms/sendCode.html', request_data, function(data) {
+                if (data.smsyzm == null) {
+                    callback(data.msg)
+                    return;
+                }
+                request_data.mobileNum = $('#phone').val()
+                request_data.smsyzm = data.smsyzm
+                $.syncPost('https://coin.jd.com/card/checkCode.html', request_data, function(data) {
+                    callback(data.resultMessage)
+                })
+            })
+        } else {
+            $.syncPost('https://coin.jd.com/card/charge.html', request_data, function(data) {
+                callback(data.resultMessage)
+            })
+        }
+
+    }
+});
+
 $(function() {
-    var cipher_no = []
+    $('#phone_line').hide()
+    var cipher_nos = []
     $('#start').click(function() {
-        if (cipher_no.length == 0) {
+        if (cipher_nos.length == 0) {
             alert('请先选择钢镚!')
             return;
         }
 
-        $.post('https://coin.jd.com/card/charge.html', { cipher_no: 'BB8A-FCEB-6564-4D31' }, function(data) {
-            console.info(data)
+        if ($('input:checked').val() == 'true' && $('#phone').val() == '') {
+            alert('请填写手机号!')
+            return;
+        }
+
+        $.each(cipher_nos, function(index, item) {
+            $('#result_' + index).html('<span style="color:red;">请稍后。。。。。。</span>')
         })
+
+        $('#start').attr('disabled', 'disabled')
+        var tempCipherNos = cipher_nos
+        var index = 0;
+        var matcher = $('#matcher').val()
+        var requestSms = $('input:checked').val() == 'true' ? true : false
+        var pid = setInterval(() => {
+            var tempCipherNo = tempCipherNos[index]
+            var cipherNo
+            if (matcher != null && matcher != undefined && matcher != '') {
+                cipherNo = tempCipherNo.substr(tempCipherNo.lastIndexOf(matcher, tempCipherNo.length) + matcher.length, tempCipherNo.length)
+            } else {
+                cipherNo = tempCipherNo
+            }
+
+            $.myPost({ cipher_no: cipherNo }, requestSms, function(msg) {
+                $('#result_' + index).html(msg)
+            })
+
+            index++
+            if (tempCipherNos.length == index) {
+                clearInterval(pid)
+                $('#start').removeAttr('disabled')
+            }
+
+        }, 2500)
+
     })
+
+    $('#reset').click(function() {
+        $('#jd_coin_info').html('')
+        cipher_nos = []
+        $('#file').val('')
+    })
+
     $('#file').change(function() {
+        $('#jd_coin_info').html('')
+        cipher_nos = []
+
         var fileInput = $('#file')
         if (!fileInput.val()) {
             alert('没有选择文件！')
@@ -24,7 +105,7 @@ $(function() {
 
         var file = document.getElementById("file").files[0];
         var reader = new FileReader();
-        reader.readAsText(file, "gb2312");
+        reader.readAsText(file);
         var jd_coin_result = ''
         reader.onload = function(e) {
             var fileText = e.target.result.split("\n");
@@ -34,12 +115,22 @@ $(function() {
                 }
 
                 jd_coin_result += ("<tr><td id = 'no'>" + (i + 1) + "</td><td id = 'coin'>" + n + "</td><td id='result_" + i + "'></td></tr>");
-                console.log(n);
-                cipher_no.push(n)
-                $('#jd_coin_info').html(jd_coin_result)
+                cipher_nos.push(n)
             });
+        }
+
+        reader.onloadend = function(e) {
+            $('#jd_coin_info').html(jd_coin_result)
         }
 
     })
 
-});
+    $(":radio").click(function() {
+        if ($('input:checked').val() == 'true') {
+            $('#phone_line').show()
+        } else {
+            $('#phone_line').hide()
+        }
+    });
+
+})
--- rob_ticket.py
+++ rob_ticket.py
@@ -1,50 +1,51 @@
 #!/usr/bin/python
-#coding:UTF-8
+# coding:UTF-8
 
 import requests
 import time
 import random
 import datetime
 
-#获取优惠券列表信息https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630
-#优惠券需要从列表信息中获取rule key,然后发送下列请求即可
+# 获取优惠券列表信息https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630
+# 优惠券需要从列表信息中获取rule key,然后发送下列请求即可
+
 
 def get_request_header(cookie):
-    requestHeaders = {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8', 
-    'Accept':'application/json, text/javascript, */*; q=0.01',
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'Referer':'https://a.jd.com/?cateId=19',
-    'Host':'a.jd.com',
-    'X-Requested-With':'XMLHttpRequest',
-    #add cookie first
-    'Cookie':cookie
-    }
+    requestHeaders = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                      'Accept': 'application/json, text/javascript, */*; q=0.01',
+                      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                      'Referer': 'https://a.jd.com/?cateId=19',
+                      'Host': 'a.jd.com',
+                      'X-Requested-With': 'XMLHttpRequest',
+                      # add cookie first
+                      'Cookie': cookie
+                      }
     return requestHeaders
 
-def ticket_rob(cookie, get_coupon_url='https://a.jd.com/indexAjax/getCoupon.html', get_cate_url='https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630'):
-    requestHeaders=get_request_header(cookie)
-    send_code_url =get_coupon_url+'?callback=jQuery1355969&key=124a025297d236857b920e3e9cf74ecacacf0bd9781c4c9dec45480ad8e35f6e3ca93b4d768649c2d493709d29b3cbea&type=1&_=1534554679960'
-    
-    # response = requests.get(send_code_url, headers=requestHeaders,allow_redirects=False)
-    # print response.text
-    target_symbol ='"success":true'
-    success_result= -1
-    # print result_str[result_str.find(target_symbol,0,len(result_str))+len(target_symbol):len(result_str)]
 
-    now_timer= datetime.datetime.now()
+def ticket_rob(cookie, rule_key, get_coupon_url='https://a.jd.com/indexAjax/getCoupon.html', get_cate_url='https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630'):
+    requestHeaders = get_request_header(cookie)
+    send_code_url = get_coupon_url+'?callback=jQuery1355969&key=' + rule_key + '&type=1&_=1534554679960'
+
+    target_symbol = '"success":true'
+    success_result = -1
+
+    now_timer = datetime.datetime.now()
     print now_timer
-    sched_timer= datetime.datetime(2018,8,18,22,00,00,000100)
+    sched_timer = datetime.datetime(2018, 8, 19, 22, 00, 00, 000100)
 
     while now_timer < sched_timer:
-        print '不相等，继续轮询,now ->',now_timer,' target ->',sched_timer
-        now_timer= datetime.datetime.now()
+        print '不相等，继续轮询,now ->', now_timer, ' target ->', sched_timer
+        now_timer = datetime.datetime.now()
     else:
         print '发送请求'
-        response = requests.get(send_code_url, headers=requestHeaders,allow_redirects=False)
+        response = requests.get(
+            send_code_url, headers=requestHeaders, allow_redirects=False)
         print response.text
-        
 
 
 if __name__ == '__main__':
     cookie = ''
-    ticket_rob(cookie)
+    # 通过getCouponListByCatalogId获得
+    rule_key = 'dd5991b1d0c347f009366643800fb3873c65ca1b8e5dc241b942e49d3eeccd7a6199d8d64d121a921cc53bbd6d535cf2'
+    ticket_rob(cookie, rule_key = rule_key)
