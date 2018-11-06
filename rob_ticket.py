f33ef1b704146807495c19a2438168e1f82d325c
facheng.wang <facheng.wang@otms.cn>
2018-11-06 20:47:57 +0800

    modify.

--- rob_ticket.py
+++ rob_ticket.py
@@ -14,8 +14,9 @@ def get_request_header(cookie):
     requestHeaders = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                       'Accept': 'application/json, text/javascript, */*; q=0.01',
                       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                      'Referer': 'https://a.jd.com/?cateId=19',
-                      'Host': 'a.jd.com',
+                    #   'Referer': 'https://a.jd.com/?cateId=19',
+                    #   'Host': 'a.jd.com',
+                      'Host':'coupon.jd.com',
                       'X-Requested-With': 'XMLHttpRequest',
                       # add cookie first
                       'Cookie': cookie
@@ -24,7 +25,10 @@ def get_request_header(cookie):
 
 
 def ticket_rob(target_timer, cookies, rule_key, get_coupon_url='https://a.jd.com/indexAjax/getCoupon.html', get_cate_url='https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630'):
-    send_code_url = get_coupon_url+'?callback=jQuery1355969&key=' + rule_key + '&type=1&_=1534554679960'
+    # send_code_url = get_coupon_url+'?callback=jQuery1355969&key=' + rule_key + '&type=1&_=1534554679960'
+
+    ##双11抢劵
+    send_code_url = "https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=297e27e0b46c4af69b2abe915a9ddf27&roleId=15012289&to=www.jd.com&"
 
     target_symbol = '"success":true'
     success_result = -1
@@ -58,11 +62,11 @@ if __name__ == '__main__':
     first_cookie = ''
     sec_cookie = ''
     cookies.append(first_cookie)
-    cookies.append(sec_cookie)
+    # cookies.append(sec_cookie)
     # 通过getCouponListByCatalogId获得
     rule_key = 'c04ea570d27dc25e7cfbc00757ba205209b5ac63df4c8170283d6b941bde7bfa649492b57fcc20b371abef1145eb2205'
 
     #150 c04ea570d27dc25e7cfbc00757ba205209b5ac63df4c8170283d6b941bde7bfa649492b57fcc20b371abef1145eb2205
     #100 3d0b0737f9ef5199730417a69e8ab16c842d53a1e4ef10962242ffd337450a3fbcc799beaf4a7ed051b1e9ca7cf786a6
-    target_timer_arg = datetime.datetime(2018, 10, 26, 16, 00, 00, 000200)
+    target_timer_arg = datetime.datetime(2018, 11, 6, 20, 42, 00, 000200)
     ticket_rob(target_timer = target_timer_arg, cookies = cookies, rule_key = rule_key)
