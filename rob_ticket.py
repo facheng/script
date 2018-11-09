75c944e225f45b9fdf14fb6def51100e669bdcb6
facheng.wang <facheng.wang@otms.com>
2018-11-09 14:28:26 +0800

    modify.

--- rob_ticket.py
+++ rob_ticket.py
@@ -10,12 +10,12 @@ import datetime
 # 优惠券需要从列表信息中获取rule key,然后发送下列请求即可
 
 
-def get_request_header(cookie):
+def get_request_header_common(cookie):
     requestHeaders = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                       'Accept': 'application/json, text/javascript, */*; q=0.01',
                       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                    #   'Referer': 'https://a.jd.com/?cateId=19',
-                    #   'Host': 'a.jd.com',
+                      'Referer': 'https://a.jd.com/?cateId=19',
+                      'Host': 'a.jd.com',
                     #   'Host':'coupon.jd.com',
                       'X-Requested-With': 'XMLHttpRequest',
                       # add cookie first
@@ -23,12 +23,28 @@ def get_request_header(cookie):
                       }
     return requestHeaders
 
+def get_request_header_11(cookie):
+    requestHeaders = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                      'Accept': 'application/json, text/javascript, */*; q=0.01',
+                      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                    #   'Host':'coupon.jd.com',
+                      'X-Requested-With': 'XMLHttpRequest',
+                      # add cookie first
+                      'Cookie': cookie
+                      }
+    return requestHeaders
 
-def ticket_rob(url, target_timer, cookies, rule_key, get_coupon_url='https://a.jd.com/indexAjax/getCoupon.html', get_cate_url='https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630', triggerNow = False):
+def get_request_header(cookie, is11rob):
+    if is11rob:
+        return get_request_header_11(cookie)
+    else:
+        return get_request_header_common(cookie)
+    
+def ticket_rob(url, target_timer, cookies, rule_key, get_coupon_url='https://a.jd.com/indexAjax/getCoupon.html', get_cate_url='https://a.jd.com/indexAjax/getCouponListByCatalogId.html?callback=jQuery4342914&catalogId=19&page=1&pageSize=9&_=1534521129630', triggerNow = False,is11rob = False):
     if triggerNow:
         print '发送请求'
         for cookie in cookies:
-            requestHeaders = get_request_header(cookie)
+            requestHeaders = get_request_header(cookie, is11rob=is11rob)
             response = requests.get(
                 url, headers=requestHeaders, allow_redirects=False)
             print response.text
@@ -50,7 +66,7 @@ def ticket_rob(url, target_timer, cookies, rule_key, get_coupon_url='https://a.j
         else:
             print '发送请求'
             for cookie in cookies:
-                requestHeaders = get_request_header(cookie)
+                requestHeaders = get_request_header(cookie=cookie, is11rob=is11rob)
                 response = requests.get(
                     url, headers=requestHeaders, allow_redirects=False)
                 print response.text
@@ -62,18 +78,26 @@ if __name__ == '__main__':
     first_cookie = ''
     sec_cookie = ''
     cookies.append(first_cookie)
+    cookies.append(sec_cookie)
     
     # cookies.append(sec_cookie)
     # 通过getCouponListByCatalogId获得
-    rule_key = 'eab4cea21752a961c8544153472518cf27d683d05cd69585b808bb951b0a724141fdd99132936d48f8aead96a1880a5a'
+    rule_key = '50dd4ddfd2d59bbbcd3a7f544aafb3636a11f0b49c95d69eff74e1fecae63464f0287689716a627c64e4470ab0835992'
 
     #150 c04ea570d27dc25e7cfbc00757ba205209b5ac63df4c8170283d6b941bde7bfa649492b57fcc20b371abef1145eb2205
     #100 3d0b0737f9ef5199730417a69e8ab16c842d53a1e4ef10962242ffd337450a3fbcc799beaf4a7ed051b1e9ca7cf786a6
 
-    # send_code_url = get_coupon_url+'?callback=jQuery1355969&key=' + rule_key + '&type=1&_=1534554679960'
+    send_code_url = 'https://a.jd.com/indexAjax/getCoupon.html?callback=jQuery1355969&key=' + rule_key + '&type=1&_=1534554679960'
 
     ##双11抢劵
-    send_code_url = "https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=297e27e0b46c4af69b2abe915a9ddf27&roleId=15012289&to=www.jd.com&"
+    #1111
+    # send_code_url = "https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=297e27e0b46c4af69b2abe915a9ddf27&roleId=15012289&to=www.jd.com&"
 
-    target_timer_arg = datetime.datetime(2018, 11, 6, 21, 15, 00, 000200)
-    ticket_rob(url = send_code_url, target_timer = target_timer_arg, cookies = cookies, rule_key = rule_key,triggerNow = True)
+    #450
+    send_code_url = "https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=9bd4537e1ad544d59ad7b2a9292ba690&roleId=15557288&to=sale.jd.com/act/fpgmdSRPst.html&"
+
+    #300
+    # send_code_url = "https://coupon.jd.com/ilink/couponSendFront/send_index.action?key=297e27e0b46c4af69b2abe915a9ddf27&roleId=15012289&to=www.jd.com&"
+
+    target_timer_arg = datetime.datetime(2018, 11, 9, 14, 00, 00, 000200)
+    ticket_rob(is11rob = True, url = send_code_url, target_timer = target_timer_arg, cookies = cookies, rule_key = rule_key,triggerNow = True)
